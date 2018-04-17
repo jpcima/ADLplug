39d87fc19395dc5462d381b47667e9a152c45306
JP Cimalando <jpcima@users.noreply.github.com>
2018-04-17 11:01:40 +0200

    update project for Projucer from develop

--- JuceLibraryCode/BinaryData.cpp
+++ JuceLibraryCode/BinaryData.cpp
@@ -321,7 +321,7 @@ static const unsigned char temp_binary_data_0[] =
 const char* knobskin_png = (const char*) temp_binary_data_0;
 
 
-const char* getNamedResource (const char* resourceNameUTF8, int& numBytes) noexcept
+const char* getNamedResource (const char* resourceNameUTF8, int& numBytes)
 {
     unsigned int hash = 0;
     if (resourceNameUTF8 != 0)
@@ -348,7 +348,7 @@ const char* originalFilenames[] =
     "knob-skin.png"
 };
 
-const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8) noexcept
+const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8)
 {
     for (unsigned int i = 0; i < (sizeof (namedResourceList) / sizeof (namedResourceList[0])); ++i)
     {
