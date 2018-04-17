0124e7965f9293b6322b52f2015a13a34cd51848
JP Cimalando <jpcima@users.noreply.github.com>
2018-04-17 04:34:43 +0200

    logarithmic vu

--- sources/ui/vu_meter.cc
+++ sources/ui/vu_meter.cc
@@ -28,18 +28,26 @@ void Vu_Meter::paint(Graphics &g)
     Rectangle<int> bounds = getLocalBounds();
     double value = value_;
 
+    double logvalue = 0;
+    if (false)
+        logvalue = value;
+    else if (value > 0) {
+        double db = 20 * log10(value);
+        const double dbmin = -60.0;
+        logvalue = (db - dbmin) / (0 - dbmin);
+    }
+
     bounds.reduce(2, 1);
     int x = bounds.getX();
     int y = bounds.getY();
     int w = bounds.getWidth();
     int h = bounds.getHeight();
 
-    // const double hue_start = 120.0 / 360.0;
-    // const double hue_range = -120.0 / 360.0;
     const double hue_start = 210.0 / 360.0;
     const double hue_range = -240.0 / 360.0;
 
-    int w2 = lround(w * value);
+    int w2 = lround(w * logvalue);
+    w2 = (w2 > w) ? w : w2;
     for (int i = 0; i < w2; ++i) {
         double r = i / (double)(w - 1);
         double hue = hue_start + r * hue_range;
