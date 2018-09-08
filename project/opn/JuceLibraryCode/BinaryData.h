ef095d4e88b06a9f7789aa9ee88e8318155ef3e0
JP Cimalando <jpcima@users.noreply.github.com>
2018-09-08 07:12:54 +0200

    update banks with a newer DMXOPN2

--- project/opn/JuceLibraryCode/BinaryData.h
+++ project/opn/JuceLibraryCode/BinaryData.h
@@ -9,7 +9,7 @@
 namespace BinaryData
 {
     extern const char*   banks_pak;
-    const int            banks_pakSize = 30251;
+    const int            banks_pakSize = 36563;
 
     extern const char*   knobskin_png;
     const int            knobskin_pngSize = 21416;
