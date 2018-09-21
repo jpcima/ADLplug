540cc420910c43f455071d49b748bffb7eb1c465
JP Cimalando <jpcima@users.noreply.github.com>
2018-09-21 11:49:31 +0200

    minor update of @Wohlstand's 4op GM bank

--- project/adl/JuceLibraryCode/BinaryData.h
+++ project/adl/JuceLibraryCode/BinaryData.h
@@ -9,7 +9,7 @@
 namespace BinaryData
 {
     extern const char*   banks_pak;
-    const int            banks_pakSize = 161637;
+    const int            banks_pakSize = 162055;
 
     extern const char*   knobskin_png;
     const int            knobskin_pngSize = 21416;
