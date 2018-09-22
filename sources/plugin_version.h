484abe255254a29fcbf2f16ea026631730e567f7
JP Cimalando <jpcima@users.noreply.github.com>
2018-09-22 18:50:52 +0200

    put the version number for the upcoming release

--- sources/plugin_version.h
+++ sources/plugin_version.h
@@ -7,5 +7,5 @@
 #include "AppConfig.h"
 
 #define ADLplug_Version JucePlugin_VersionString
-#define ADLplug_VersionExtra "Beta 1"
-#define ADLplug_SemVer JucePlugin_VersionString "-beta.1"
+#define ADLplug_VersionExtra "Beta 2"
+#define ADLplug_SemVer JucePlugin_VersionString "-beta.2"
